4b87b6d48463fe491df22413294b682dbd4d4664
wangpengxpy <13682687785@163.com>
2016-11-23 23:18:11 +0800

    FineUploader上传例子

---
+++ FineUploader/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FineUploader
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FineUploader/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace FineUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProcessUpload(HttpPostedFileWrapper qqfile)
+        {
+            string extension = qqfile.ContentType.Split('/')[1];
+            if ((extension == "jpeg") || (extension == "jpg") || (extension == "gif") || (extension == "png"))
+            {
+                using (var inputstream = Request.InputStream)
+                {
+                    using (var filestream = new FileStream(@"d:\temp\" + qqfile.FileName, FileMode.Create))
+                    {
+                        inputstream.CopyTo(filestream);
+                    }
+                }
+                return Json(new { success = true });
+
+            }
+            else {
+                return Json(new { success = false, message = qqfile.FileName + "has an invalid extension. Valid extension(s): jpeg, jpg, gif, png." });
+            }
+
+           
+        }
+    }
+}
